783a857f058b619c0d17c4aec226b5f030046933
Kristian.sk <Kristian.sk@hotmail.com>
2025-09-06 21:16:32 +0200

    Can now update image Added JS to display the product table with category

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
+
 namespace BulkyBookWeb.Areas.Admin.Controllers;
 
 [Area("Admin")]
@@ -18,7 +19,7 @@ public class ProductController : Controller
     }
     public IActionResult Index()
     {
-        List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+        List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
 
         return View(objProductList);
     }
@@ -62,6 +63,17 @@ public class ProductController : Controller
                 string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                 string productPath = Path.Combine(wwwRootPath, @"images\product");
                 
+                if(!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                {
+                    //Delete the old image
+                    string oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                    if(System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+
+                }
+
                 using (FileStream fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                 {
                     file.CopyTo(fileStream);
@@ -70,7 +82,15 @@ public class ProductController : Controller
                 productVM.Product.ImageUrl = @"\images\product\" + fileName;
             }
 
-            _unitOfWork.Product.Add(productVM.Product);
+            if(productVM.Product.Id == 0)
+            {
+                _unitOfWork.Product.Add(productVM.Product);
+            }
+            else
+            {
+                _unitOfWork.Product.Update(productVM.Product);
+            }
+
             _unitOfWork.Save();
             TempData["success"] = "Product created successfully";
             return RedirectToAction("Index");
@@ -116,4 +136,12 @@ public class ProductController : Controller
         TempData["success"] = "Product deleted successfully";
         return RedirectToAction("Index");
     }
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+        return Json(new { data = objProductList });
+    }
+    #endregion
 }
